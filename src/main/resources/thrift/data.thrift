d01728caf654e91a4430d3b00bc7a2c31a096316
kun great <mydreams2018@outlook.com>
2019-04-15 23:19:11 +0800

    18

---
+++ src/main/resources/thrift/data.thrift
@@ -0,0 +1,21 @@
+namespace java cn.kungreat.netty.domain
+typedef i32 int
+typedef i64 long
+typedef i32 int
+
+struct User{
+    1:optional string name,
+    2:optional int age,
+    3:required int id
+}
+
+exception DataException{
+       1:required string messgae,
+       2:required bool success,
+       3:required string date
+}
+
+service UserService{
+    User getUserById(1:required int id) throws  (1: DataException dataException),
+    void saveUser (1:required User user) throws (1: DataException dataException)
+}
